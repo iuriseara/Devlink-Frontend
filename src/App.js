a7ba6a32c41ae1a63b29e1b1c689809eddee4ad9
iuriseara <iuriseara@gmail.com>
2021-01-26 15:03:37 -0500

    Added Desktop Landing Page & Styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Fragment } from 'react'
 import Navbar from './components/layout/Navbar/Navbar'
+import Landing from './components/layout/LandingPage/Landing'
 import './App.css';
 
 function App() {
   return (
     <Fragment>
       <Navbar />
+      <Landing />
     </Fragment>
   );
 }
--- src/components/layout/LandingPage/Landing.jsx
+++ src/components/layout/LandingPage/Landing.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Landing.scss';
+import logoText from '../../../img/logo/logo-text.svg';
+import landingGfx from '../../../img/icons/landingIcon.svg';
 
-export const Landing = () => {
+export default function Landing() {
     return (
-        <div>
-            
+        <div className='landing-page'>
+            <div className="text-wrapper">
+                <img src={logoText} className="logo-text"/>
+                <h1 className='landing-header-text'>The First Social Media Platform built by Developers. For Developers.</h1>
+                <button className='landing-signup-btn'>Sign Up</button>
+            </div>
+
+            <div className="right-wrapper">
+                <img src={landingGfx} class='landing-gfx'/>
+            </div>
         </div>
     )
 }
